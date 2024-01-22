9847e32e63925912d24afce98f8815e44c496e6e
shivanitrivedi123 <shivani.trivedi@gtcsys.com>
2024-01-22 11:50:41 +0530

    feat: # - Changes In The Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { ReactComponent as LinkedIn } from "../assets/LinkedIn.svg";
+import { ReactComponent as Github } from "../assets/Github.svg";
+import { ReactComponent as Mail } from "../assets/Mail.svg";
+import { Link } from "react-scroll";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <div className="text-4xl font-bold text pt-8 mb-10">
+        <span>Get In </span>
+        <span className="text-blue-400">Touch</span>
+      </div>
+      <div className="flex items-center justify-center mb-16">
+        <a href="https://github.com/shivani18051996">
+          <LinkedIn className="w-16 h-16 mr-7"/>
+        </a>
+        <a href="https://github.com/shivani18051996">
+          <Github className="w-16 h-16 mr-7" />
+        </a>
+        <a href="">
+          <Mail className="w-16 h-16" />
+        </a>
+      </div>
+      <div className="copyright text-xl font-bold text pb-10">
+        <span> Made By </span>
+        <span className="text-blue-400"> SHIVANI TRIVEDI</span>
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/MyProject.js
+++ src/components/MyProject.js
@@ -1,7 +1,7 @@
 const MyProject = () => {
   const posts = [
     {
-      title: "Netflix",
+      title: "Netflix App",
       img: "/images/netflix-clone.png",
       content:
         "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content",
@@ -10,41 +10,48 @@ const MyProject = () => {
       title: "Food Ordering App",
       img: "/images/food-ordering-app.png",
       content:
-        "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content",
-    },
-    {  
-      title: "React Tailwind Card with Grid 3",
-      img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
-      content:
-        "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content",
+        " A Food Ordering App that display the list of food Items , having the functionality ADD TO CART , CHECKOUT , FILTER ITEMS , SEARCH ITEMS",
     },
     {
-      title: "React Tailwind Card with Grid 4",
-      img: "https://cdn.pixabay.com/photo/2019/12/17/14/43/christmas-4701783__340.png",
+      title: "StopWatch App",
+      img: "/images/stopwatch.png",
       content:
-        "react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content",
+        " A stopwatch is a timekeeping device with start, stop, and reset functionalities, measuring elapsed time from the moment it's started.",
+    },
+    {
+      title: "Timer App",
+      img: "/images/timer.png",
+      content:
+        "A Timer App typically includes start, pause, resume, and reset functionalities, enabling users to manage and monitor elapsed time.",
     },
   ];
   return (
     <>
-    <div className="text-4xl font-bold text pt-8 mb-10"><span>My </span>
-    <span className="text-blue-400">Projects</span></div>
-      <div className="grid gap-2 lg:grid-cols-4">
+      <div className="text-4xl font-bold text pt-8 mb-10">
+        <span>My </span>
+        <span className="text-blue-400">Projects</span>
+      </div>
+      <div className="grid gap-2 lg:grid-cols-4 mb-24">
         {posts.map((items, key) => (
           <div className="w-full rounded-lg shadow-md lg:max-w-sm" key={key}>
             <img
               className="object-cover w-full h-48"
               src={items.img}
-              alt="image"
+              alt={items.title}
             />
             <div className="p-4">
               <h4 className="text-xl font-semibold text-blue-600">
                 {items.title}
               </h4>
               <p className="mb-2 leading-normal">{items.content}</p>
-              <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
-                Read more
-              </button>
+              <div className="">
+                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow mr-4">
+                  Live Link
+                </button>
+                <button className="px-4 py-2 text-sm text-blue-100 bg-blue-500 rounded shadow">
+                  Code Link
+                </button>
+              </div>
             </div>
           </div>
         ))}
